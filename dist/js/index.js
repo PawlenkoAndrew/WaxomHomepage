18c254680aa737ba032d5642369183abdc4a4834
PawlenkoAndrew <versa1661@gmail.com>
2019-04-05 14:30:27 +0300

    v 2.0
    v 2.0

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,11 +1,52 @@
-(function($){
-  $(function() {
-    $('.menu__icon').on('click', function() {
-      $(this).closest('.main-nav').toggleClass('menu_state_open');
-    });
-  });
-})(jQuery);
 
-function viewDiv(){
-  document.getElementById("form").style.display = "block";
-};
+
+
+$('.title-holder').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows:false
+      }
+    }
+  ]
+});
+  
+
+
+function viewDiv(id) {
+        var el = document.getElementById(id);
+        var link = document.getElementById('search');
+        if (el.style.display == "block") {
+            el.style.display = "none";
+            
+        } else {
+            el.style.display = "block";
+           
+        }
+    }
+
+$(function(){
+
+  $('.menu__icon').on('click', function(){
+
+      $('.menu__links').slideToggle(300,function(){
+
+        if($ (this) .css('display') ==='none'){
+          $(this) .removeAttr('style');
+        }
+
+      });
+
+  });
+
+})
